e3f7ce044e34ebe3fc0aeaab0ef6ffbb38d2e5ad
Bruno Dutra <brunodutra.ho@gmail.com>
2024-01-22 17:02:33 -0300

    Slide section hero]

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,17 @@
+//Paginador slides
+var swiper = new Swiper(".mySwiper", {
+    effect: 'fade',
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+  });
+
+
+//Modal detalhes do pokemon
 const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
 const closePokemon = document.querySelector('.js-close-modal-details-pokemon')
 const closeOverlay = document.querySelector('.js-overlay-modal-details-pokemon')
@@ -16,3 +30,5 @@ cardPokemon.forEach(card => {
 
 closePokemon.addEventListener('click', closeDetailsPokemon);
 closeOverlay.addEventListener('click', closeDetailsPokemon);
+
+
