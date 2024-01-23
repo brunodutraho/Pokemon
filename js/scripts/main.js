1cfd8952b2943a092e0c1c2bcdbb2f661cbf5277
Bruno Dutra <brunodutra.ho@gmail.com>
2024-01-23 07:24:43 -0300

    Responsivo header e section hero

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -4,10 +4,7 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-    },
+    
   });
 
 
